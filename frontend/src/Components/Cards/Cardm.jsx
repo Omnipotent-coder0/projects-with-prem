0ae93935586421f5861dfcf85d862168b974ce84
Nilesh Gautam <nileshgautam13092001+git@gmail.com>
2024-02-26 22:55:01 +0530

    added entetainment section and minor styling

--- frontend/src/Components/Cards/Cardm.jsx
+++ frontend/src/Components/Cards/Cardm.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react'
 import NoImage from '../../assets/noimage.png'
 const Cardm = (props) => {
   return (
-    <div className='max-w-96 bg-slate-100 hover:cursor-pointer hover:shadow-lg duration-200'>
-      <img className="rounded-t-xl  aspect-video" src={ props.item.urlToImage ? props.item.urlToImage : NoImage} alt="" />
+    <div className='max-w-96 rounded-xl border-2 overflow-hidden border-slate-200 bg-slate-100 hover:cursor-pointer hover:shadow-lg duration-200'>
+      <img className="  aspect-video" src={ props.item.urlToImage ? props.item.urlToImage : NoImage} alt="" />
       <div className='m-4' >
         <h2 className='font-bold text-slate-900 text-xl h-14 overflow-hidden'>{props.item.title}</h2>
         <h4 className='leading-6 h-12 overflow-hidden'>{props.item.description}</h4>
