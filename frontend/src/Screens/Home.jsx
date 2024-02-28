d2976942401cccdc596fa37e7199489a6c17de67
Nilesh Gautam <nileshgautam13092001+git@gmail.com>
2024-02-28 13:57:15 +0530

    Added curves to the main screen

--- frontend/src/Screens/Home.jsx
+++ frontend/src/Screens/Home.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import Hot from '../Components/Hot'
 import Row from '../Components/Row'
-
+import greenWave from '../assets/greenWave.svg'
+import blueWave from '../assets/blueWave.svg'
+import greyWave from '../assets/greyWave.svg'
+import purpleWave from '../assets/purpleWave.svg'
+import purpleWaveEnd from '../assets/purpleWaveEnd.svg'
 const Home = () => {
   return (
     <div>
         <Hot route = {'trending'}/>
-        <Row route = {'health'}/>
-        <Row route = {'technology'}/>
-        <Row route = {'business'}/>
-        <Row route = {'entertainment'}/>
+        <img className='w-full' src={greenWave} alt="" />
+        <div className="bg-lime-600 ">
+          <Row route = {'health'}/>
+        </div>
+        <img className='w-full' src={blueWave} alt="" />
+        <div className="bg-cyan-700">
+          <Row route = {'technology'}/>
+        </div>
+        <img className='w-full' src={greyWave} alt="" />
+        <div className="bg-gray-500">
+          <Row route = {'business'}/>
+        </div>
+        <img className='w-full' src={purpleWave} alt="" />
+        <div className="bg-purple-500">
+          <Row route = {'entertainment'}/>
+        </div>
+        <img className='w-full' src={purpleWaveEnd} alt="" />
     </div>
   )
 }
